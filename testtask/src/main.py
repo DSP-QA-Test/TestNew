5c87360187092a0917fbaee0b0afe5c2ea7d9606
Melnychuk-Andrii <androkep@gmail.com>
2022-02-09 17:12:13 +0200

    2 more try

--- testtask/src/main.py
+++ testtask/src/main.py
@@ -3,19 +3,21 @@ import os
 
 def create_argument_parser():
     parser = argparse.ArgumentParser(description = 'GenerateFile creates a random file of a given size')
-    parser.add_argument('--filename', nargs = '?', required = True, help = 'The name of the file to create')
-    parser.add_argument('--filesize', nargs = '?', required = True, help = 'The size of the file to create in bytes.', type = int)
+    parser.add_argument('--filename', nargs = '?', required = False, help = 'The name of the file to create')
+    parser.add_argument('--filesize', nargs = '?', required = False, help = 'The size of the file to create in bytes.', type = int)
     return parser
 
 def generate_file(filename, filesize):
-    command = 'head -c {0} < /dev/urandom > {1}'.format(filesize, filename);
+	filename = 'name'
+	filesize = 128
+    command = 'head -c {0} < /dev/urandom > {1}'.format(filesize, filename)
 
-    print('Generating new random file with:');
-    print(command);
+    print('Generating new random file with:')
+    print(command)
 
-    os.system(command);
+    os.system(command)
 
 if __name__ == "__main__":
     parser = create_argument_parser()
     args = parser.parse_args()
-    generate_file(args.filename, args.filesize);
+    generate_file(args.filename, args.filesize)
